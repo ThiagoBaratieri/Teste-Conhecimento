89c09a01b532e4110be2ee4d1af68a439e04ec32
Thiago Baratieri <thiagobaratieri971@gmail.com>
2024-09-14 01:49:36 -0300

    Resposta desafio 2 - Python

--- testPython.py
+++ testPython.py
@@ -14,3 +14,15 @@ df = pd.DataFrame(data)
 totalVendasCliente = df.groupby('Cliente_ID')['Preco_Total'].sum().reset_index()
 totalVendasCliente = totalVendasCliente.sort_values(by='Preco_Total', ascending=False) # deixar em ordem DESC
 print(totalVendasCliente) # utilizei o metodo groupby() para agrupar os clientes pelo id
+
+print('\n')
+# 2 - Escreva uma função em Python que recebe um DataFrame e retorna um novo DataFrame onde todos 
+# os valores da coluna Preco_Total sejam multiplicados por 1.1 (um aumento de 10%).
+
+def newDf(df):
+    novoDf = pd.DataFrame(df).copy() # copio o dataframe que irei receber na funçao
+    novoDf['Preco_Total'] = novoDf['Preco_Total'] * 1.1 # adiciono 10% na coluna Preco_Total :)
+    print(novoDf)
+
+
+newDf(data)
